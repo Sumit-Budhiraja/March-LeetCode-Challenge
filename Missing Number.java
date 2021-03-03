26b2d08962663a945b5bc12e31a0ad1c47ad7e75
Sumit Budhiraja <48406445+Sumit-Budhiraja@users.noreply.github.com>
2021-03-04 00:30:52 +0530

    Add files via upload

---
+++ Missing Number.java
@@ -0,0 +1,9 @@
+class Solution {
+public int missingNumber(int[] nums) {
+        int result = 0;
+        for(int i = 0; i < nums.length; i++){
+            result += (i + 1 - nums[i]);
+        }
+        return result;
+    }
+}

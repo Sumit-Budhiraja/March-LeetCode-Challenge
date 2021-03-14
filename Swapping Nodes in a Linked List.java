a1a52d7acf2c51600a272e6822ce5baf5e7a394b
Sumit Budhiraja <48406445+Sumit-Budhiraja@users.noreply.github.com>
2021-03-15 01:19:36 +0530

    Add files via upload

---
+++ Swapping Nodes in a Linked List.java
@@ -0,0 +1,27 @@
+class Solution {
+    // TC : O(n)
+    // SC : O(1)
+    public ListNode swapNodes(ListNode head, int k) {
+        k = k-1;
+        ListNode firstN = head;
+        while(k>0){
+            firstN = firstN.next;
+            k--;
+        }
+
+        ListNode f = firstN;
+        ListNode secondN = head;
+        while(f.next!=null){
+            secondN = secondN.next;
+            f = f.next;
+        }
+
+        int temp = firstN.val;
+        firstN.val = secondN.val;
+        secondN.val = temp;
+
+        return head;
+
+
+    }
+}
